f0fb17e0d08cc9431271cec481327fda1ef1f2ad
Don Peat <don@donaldpeat.com>
2018-02-11 19:28:16 -0800

    feature: add contact & projects pages & routing

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>
+          put contact stuff here
+          <a href="mailto:don@donaldpeat.com">email</a>.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+import Home from './Home';
+import Projects from './Projects';
+import Contact from './Contact';
 
 class Main extends Component {
   render() {
     return (
-      <div>
-        <h1>HEADER TEST</h1>
-        <ul className="header">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/stuff">Stuff</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-        <div className="content" />
-      </div>
+      <HashRouter>
+        <div>
+          <h1>HEADER TEST</h1>
+          <ul className="header">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+          <div className="content">
+            <Route path="/" component={Home} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
